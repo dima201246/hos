9e41cd5a83724701a9c5cc95225caafc308daeea
Dmitriy.Volchenko <dima201246@gmail.com>
2016-02-03 04:44:02 +0300

    Again testing menu_win, more functional

--- bootloader/bootloader.cpp
+++ bootloader/bootloader.cpp
@@ -48,15 +48,39 @@ int main(int argc, char *argv[]) {
 	teststr.title = "Pre pre pre ... Alpha";
 	teststr.style = 1;
 	teststr.line = "Dear user, it's not full version of OS!/nThis is just an example of how might look this OS.";
-	// msg_win(teststr);
+	msg_win(teststr);
+	erase();
 	
 	/*DEBUG*/
 	vector <string> items;
+	teststr.style = 0;
 	teststr.border_menu = true;
 	teststr.selected = 2;
+	teststr.ymax = 5;
 	items.insert(items.end(), "test");
 	items.insert(items.end(), "test2");
+	items.insert(items.end(), "test3");
+	items.insert(items.end(), "test4");
+	items.insert(items.end(), "test5");
+	items.insert(items.end(), "test6");
+	items.insert(items.end(), "test7");
+	items.insert(items.end(), "test8");
+	items.insert(items.end(), "test9");
+	items.insert(items.end(), "test10");
+
+	int key = 0;
+	while (key != 27) {
+		menu_win(teststr, items);
+		key = getch();
+		switch (key) {
+			case KEY_UP: if (teststr.selected != 0) teststr.selected--; break;
+			case KEY_DOWN: if (teststr.selected != 10) teststr.selected++; break;
+		}
+	}
+	teststr.xpos = teststr.xreturn;
+	teststr.ypos = teststr.yreturn;
 	menu_win(teststr, items);
+	
 	mvprintw(10, 10, "%i", teststr.xreturn);
 	mvprintw(11, 10, "%i", teststr.yreturn);
 	getch();
