a666930174404a8caed39fe454f1cff0de5cb807
viktor <viktorokh96@mail.ru>
2016-02-04 17:02:27 +0300

    Теперь всё работает
    Подышав краской я подумал, что функция stat не всеядная и запихнул более правильный путь  файлам, и всё заработало , тоесть вместо .<filename> нужно передавать ./<filename>. В будущем можно замутить мини-парсер для проверки правильности, по крайней мере после того как дома перестанет вонять краской

--- apps/file_man/file_man_main.cpp
+++ apps/file_man/file_man_main.cpp
@@ -16,14 +16,14 @@ int main(int argc, char* argv[]) {
 	vector <FILEINFO> test_vec;
 	FILEINFO test_str;
 	// get_files("./test", test_vec);
-	get_files(".", test_vec);
+	get_files("./", test_vec); // !!! Для stat()
 	/*for(unsigned int i = 0; i < test_vec.size(); i++) {
 		test_str = test_vec[i];
 		printw("%s\n", test_str.name.c_str());
 	}*/
 
 	printw("\n");
-	files_sort_by('n', test_vec);
+	files_sort_by('t', test_vec);
 	for(unsigned int i = 0; i < test_vec.size(); i++) {
 		test_str = test_vec[i];
 		printw("%i %s %s\n", (int)test_str.name[0], test_str.name.c_str(), ctime(&test_str.mtime));
