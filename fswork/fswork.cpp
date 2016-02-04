f4a7726c829d6f6f147ea70074999632d2a4dc8a
viktor <viktorokh96@mail.ru>
2016-02-04 15:30:17 +0300

    Не работает сортировка по времени, дело не в алгоритме
    С алгоритмов всё ништяк, вроде, но данные времени, передаваемые во время каждой итерации в цикле получения файлов, почему то оказываются одинаковыми....

--- fswork/fswork.cpp
+++ fswork/fswork.cpp
@@ -11,8 +11,14 @@
 #include "../configurator/configurator.h"
 #include "fswork.h"
 #include <curses.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <cstring>
+
 
 using namespace std;
+
 
 bool makedir(string path) {
 	/*if (mkdir(path.c_str(), 0777) != 0) {
@@ -30,9 +36,9 @@ void files_sort_by(char type_sort, vector<FILEINFO> &filevec) {
 	unsigned int k_cycle, itemp_1, itemp_2;
 	for (unsigned int i = 0; i < filevec.size() - 1; i++) { // Сортировка методом пузырька
 		for (unsigned int j = 0; j < filevec.size() - i - 1; j++) {
+			temp_1 = filevec[j];
+			temp_2 = filevec[j + 1];
 			if (type_sort == 'n') { // Сортировка по имени
-				temp_1 = filevec[j];
-				temp_2 = filevec[j + 1];
 				if (llength(temp_1.name) >= llength(temp_2.name)) k_cycle = llength(temp_2.name); // Взятие длины наименьшего
 				else k_cycle = llength(temp_1.name);
 				for (unsigned int k = 0; k < k_cycle; k++) { // Чтобы сортировка шла не только по первым буквам, но и по остальным, если они одинаковые
@@ -50,6 +56,13 @@ void files_sort_by(char type_sort, vector<FILEINFO> &filevec) {
 				}
 
 			}
+			if (type_sort == 't') { // Сортировка по времени
+				if (temp_2.mtime < temp_1.mtime) {
+					temp_3 = filevec[j];
+					filevec[j] = filevec[j + 1];
+					filevec[j + 1] = temp_3;		
+				}
+			}
 		}	
 	}
 	return;
@@ -62,10 +75,15 @@ int get_files(string path, vector<FILEINFO> &filevec) {
 	else {
 		FILEINFO file_info;
 		struct dirent *f_cur;
+		struct stat fs_cur;						
+		string tmp;
 		while ((f_cur=readdir(dir))!= NULL) {
 			file_info.name = f_cur->d_name;
-			if ((file_info.name != "..") && (file_info.name != ".")) {
-				filevec.insert(filevec.end(), file_info);
+			tmp = path + (char *) f_cur -> d_name;		// Получаем полный адрес файла для stat()
+			stat(tmp.c_str(), &fs_cur); 				// Получение информации о файле 
+			file_info.mtime = fs_cur.st_mtime;			// Сохраняем время последней модификации
+			if ((file_info.name != "..") && (file_info.name != ".")) { 
+				filevec.insert(filevec.end(), file_info);  // Костылей нет. p.s честно p.s.s ну почти...
 			}
 		}
 		closedir(dir);
