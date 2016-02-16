19dea169b8d1b67a4db210b17c6a957bd6fd2a90
Dmitriy.Volchenko <dima201246@gmail.com>
2016-02-17 01:45:42 +0300

    Added menu algorithm

--- desktop/desktop.cpp
+++ desktop/desktop.cpp
@@ -10,13 +10,37 @@
 
 using namespace std;
 
-string local_user;
+void open_menu(vector <string>& app_list) {
+	DLGSTR menu_panel = {};
+	menu_panel.xpos = 1;
+	menu_panel.ypos = 1;
+	// menu_panel.style = 3;
+	menu_panel.border_menu = true;
+	bool cycle = true;
+	int key;
+	while (cycle) {
+		menu_win(menu_panel, app_list);
+		key = getch();
+		switch (key) {
+			case 27: cycle = false; break;
+			case KEY_UP: if (menu_panel.selected != 1) menu_panel.selected--; break;
+			case KEY_DOWN: if (menu_panel.selected != menu_panel.second_border) menu_panel.selected++; break;
+			/*case '\n': switch (menu_panel.selected) {
+							case 1: abaut_fm(); break;
+							case 2: return; break;
+						} break;*/
+		}
+	}
+	return;
+}
 
 int draw_desktop(int selected, bool open_label, bool new_draw, unsigned int maxX, unsigned int maxY) {
 	unsigned x, y;
 	if (new_draw) {
-		for (x = 15; x < maxX; x++) mvprintw(0, x, "-");
-		for (x = 0; x < maxX; x++) mvprintw(maxY - 1, x, "-");
+		for (x = 0; x < maxX; x++) {
+			mvprintw(maxY - 1, x, "-");
+			mvprintw(0, x, "-");
+		}
 		for (y = 0; y < maxY; y++) {mvprintw(y, 0, "|"); mvprintw(y, maxX - 1, "|");}
 	}
 		if (selected == 0) attron(COLOR_PAIR(1) | A_BOLD); // Белый цвет
@@ -31,7 +55,7 @@ int draw_desktop(int selected, bool open_label, bool new_draw, unsigned int maxX
 	return 0;
 }
 
-int work_desktop() {
+int work_desktop(vector <string> app_list, string user_name) {
 	unsigned int maxY, maxYb = 0, maxX, maxXb = 0;
 	bool cycle = true, open_label = false, new_draw = false;
 	int key_pressed, selected = 2;
@@ -49,8 +73,8 @@ int work_desktop() {
 			case KEY_RIGHT: if (selected != 2) selected++; break;
 			case KEY_LEFT: if (selected != 0) selected--; break;
 			case '\n': switch (selected) {
+							case 0: open_menu(app_list); break;
 							case 2: cycle = false; break;
-							// default: warning_win("Ooops... It's not work...", 0); /*open_label = true;*/ break;
 						}
 			case 27: if (open_label) open_label = false;
 					/*else shutdown_process();*/
@@ -61,10 +85,8 @@ int work_desktop() {
 }
 
 int main_desktop(string user_name/*...*/) {
-	local_user = user_name;
-	work_desktop();
-	/*vector <string> app_list;
-	get_apps_list(app_list);*/
-	getch();
+	vector <string> app_list;
+	get_apps_list(app_list);
+	work_desktop(app_list, user_name);
 	return 0;
 }
