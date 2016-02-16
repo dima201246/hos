26dc4f15fcea2539689bdd9970dd8b97d73399e1
Dmitriy.Volchenko <dima201246@gmail.com>
2016-02-17 01:46:45 +0300

    Multiple fixes and additions

--- desktop/header/menu_apps.cpp
+++ desktop/header/menu_apps.cpp
@@ -1,23 +1,34 @@
 #include "../../configurator/configurator.h"
 #include "../../fswork/fswork.h"
+#include "../../lang/lang.h"
+#include "../../apps_starter/apps_starter.h"
 #include "menu_apps.h"
 
-#define APPS_FILE "./apps/apps.list"
-#define APPS_DIR "./apps/" // Внимание, всегда должна оканчиваться слешем!!!
+#include <stdlib.h>
 
 using namespace std;
 
 int get_apps_list(vector<string>& names) {
 	names.clear();
+	unsigned int count = 0;
 	if (!FileExists(APPS_FILE)) {
 		vector <FILEINFO> apps_vec;
 		get_files(APPS_DIR, apps_vec); // Получение списка папок с (предполагается) приложениями 
 		for (unsigned int i = 0; i < apps_vec.size(); i++) {
 			if ((FileExists(APPS_DIR + apps_vec[i].name + "/app.config")) && (configurator(APPS_DIR + apps_vec[i].name + "/app.config", "type", "", false) == "app")) {
-				add_to_file(APPS_FILE, configurator(APPS_DIR + apps_vec[i].name + "/app.config", "name_package", "", false));
+				count++;
+				add_to_file(APPS_FILE, str(count) + "_app_package_name = \"" + configurator(APPS_DIR + apps_vec[i].name + "/app.config", "name_package", "", false) + "\"");
+				add_to_file(APPS_FILE, str(count) + "_app_path = \"" + APPS_DIR + apps_vec[i].name + "\"");
+				add_to_file(APPS_FILE, str(count) + "_app_launcher = \"" + configurator(APPS_DIR + apps_vec[i].name + "/app.config", "launcher", "", false) + "\"");
 			}
 		}
-		load_to_vector(APPS_FILE, names);
-	} else load_to_vector(APPS_FILE, names);
+		add_to_file(APPS_FILE, "all_apps = " + str(count));
+		for (unsigned int i = 1; i <= count; i++)
+			names.insert(names.end(), configurator(APPS_FILE, str(i) + "_app_package_name", "", false));
+	} else {
+		count = atoi(configurator(APPS_FILE, "all_apps", "", false).c_str());
+		for (unsigned int i = 1; i <= count; i++)
+			names.insert(names.end(), configurator(APPS_FILE, str(i) + "_app_package_name", "", false));
+	}
 	return 0;
 }
