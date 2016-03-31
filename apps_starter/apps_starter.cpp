2a72c07b4806443ef9fdf3fec9327c543f5ad500
Dmitriy Volchenko <dima201246@gmail.com>
2016-03-31 05:09:07 +0300

    Clear

--- apps_starter/apps_starter.cpp
+++ apps_starter/apps_starter.cpp
@@ -2,6 +2,7 @@
 #include "../lang/lang.h"
 #include "../configurator/configurator.h"
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -11,8 +12,15 @@ using namespace std;
 
 int app_start(int number_of_app, const char* parametrs) {
 	string full_path = configurator(APPS_FILE, str(number_of_app) + "_app_path", "", false) + "/" + configurator(APPS_FILE, str(number_of_app) + "_app_launcher", "", false);
-	execl(full_path.c_str(), parametrs, NULL);
-	// fork(full_path.c_str(), parametrs, NULL);
-	printw("%s", full_path.c_str()); getch();
+	erase();
+	/*TEST*/
+	pid_t pid = fork();
+	if (pid == 0)
+		execl(full_path.c_str(), parametrs, NULL); // parent process
+	else {
+		erase();
+		refresh();
+	}
+	/*TEST*/
 	return 0;
 }
