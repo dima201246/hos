bc4d5d2e621f72115d5bdd9e7c5457eef270a186
Dmitriy Volchenko <dima201246@gmail.com>
2016-06-11 20:26:04 +0300

    Proprietary library

--- configurator/configurator.h
+++ configurator/configurator.h
@@ -1,8 +1,15 @@
 #ifndef CONFIGURATOR_H
 #define CONFIGURATOR_H
-	#include <string>
+
+	#include <fstream>
 	#include <vector>
-	std::string configurator(std::string, std::string, std::string, bool);
-	std::string add_to_file(std::string, std::string);
-	std::string load_to_vector(std::string, std::vector <std::string>&);
+	#include <string>
+	#include <stdlib.h>
+
+	std::string cl_version(); // Версия библиотеки
+	std::string configurator(std::string /*Путь к файлу*/, std::string /*Искомый параметр*/, std::string /*Новое значение, если нужна перезапись*/, bool /*Перезапись значения или нет*/); // Поиск в файле нужного параметра и возврат его значения
+	void add_to_file(std::string /*Путь к файлу*/, std::string /*Строка, которая будет добавлена*/); // Запись в конец файла
+	std::string conf(std::string /*Искомый параметр*/, std::vector<std::string> /*вектор со значениями*/); //Поиск в векторе нужного параметра и возврат его значения
+	bool load_to_vector(std::string /*Имя файла*/, std::vector<std::string>&  /*Ссылка на вектор*/); // Загрузка файла в вектор
+
 #endif
