c26166a9c470abaac9f65d2f014ddbd068f15498
Dmitriy Volchenko <dima201246@gmail.com>
2016-06-11 19:55:28 +0300

    libscreen->screen

--- apps_starter/apps_starter.h
+++ apps_starter/apps_starter.h
@@ -1,6 +1,14 @@
 #ifndef APPS_STARTER_H
 #define APPS_STARTER_H
+
+	#include <wait.h>
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <unistd.h>
+
 	#define APPS_FILE "./apps/apps.list"
 	#define APPS_DIR "./apps/" // Внимание, всегда должна оканчиваться слешем!!!
+
 	int app_start(int, const char*);
+
 #endif
