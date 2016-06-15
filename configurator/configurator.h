4538d6a88343267bb35b24bcc326d79a23c75c19
Dmitriy Volchenko <dima201246@gmail.com>
2016-06-15 16:16:21 +0300

    Ничего интересного

--- configurator/configurator.h
+++ configurator/configurator.h
@@ -6,7 +6,6 @@
 	#include <string>
 	#include <stdlib.h>
 
-	std::string cl_version(); // Версия библиотеки
 	std::string configurator(std::string /*Путь к файлу*/, std::string /*Искомый параметр*/, std::string /*Новое значение, если нужна перезапись*/, bool /*Перезапись значения или нет*/); // Поиск в файле нужного параметра и возврат его значения
 	void add_to_file(std::string /*Путь к файлу*/, std::string /*Строка, которая будет добавлена*/); // Запись в конец файла
 	std::string conf(std::string /*Искомый параметр*/, std::vector<std::string> /*вектор со значениями*/); //Поиск в векторе нужного параметра и возврат его значения
