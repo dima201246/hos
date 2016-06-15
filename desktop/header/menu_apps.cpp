a256148409ae89a4cba9447bd523024dcd748a80
Dmitriy Volchenko <dima201246@gmail.com>
2016-06-15 16:31:31 +0300

    Переделка способа создание и чтения списка приложений

--- desktop/header/menu_apps.cpp
+++ desktop/header/menu_apps.cpp
@@ -11,6 +11,7 @@ using namespace std;
 int get_apps_list(vector<string>& names) {
 	names.clear();
 	unsigned int count = 0;
+	vector <string> apps_file_vec;
 	if (!FileExists(APPS_FILE)) {
 		vector <FILEINFO> apps_vec;
 		get_files(APPS_DIR, apps_vec); // Получение списка папок с (предполагается) приложениями 
@@ -18,18 +19,22 @@ int get_apps_list(vector<string>& names) {
 		for (unsigned int i = 0; i < apps_vec.size(); i++) {
 			if ((FileExists(APPS_DIR + apps_vec[i].name + "/app.config")) && (configurator(APPS_DIR + apps_vec[i].name + "/app.config", "type", "", false) == "app")) {
 				count++;
-				add_to_file(APPS_FILE, str(count) + "_app_package_name = \"" + configurator(APPS_DIR + apps_vec[i].name + "/app.config", "name_package", "", false) + "\"");
+				
+				load_to_vector(APPS_DIR + apps_vec[i].name + "/app.config", apps_file_vec); // Загрузка файла с описанием приложения
+				
+				add_to_file(APPS_FILE, str(count) + "_app_package_name = \"" + conf("name_package", apps_file_vec) + "\"");
 				add_to_file(APPS_FILE, str(count) + "_app_path = \"" + APPS_DIR + apps_vec[i].name + "\"");
-				add_to_file(APPS_FILE, str(count) + "_app_launcher = \"" + configurator(APPS_DIR + apps_vec[i].name + "/app.config", "launcher", "", false) + "\"");
+				add_to_file(APPS_FILE, str(count) + "_app_launcher = \"" + conf("launcher", apps_file_vec) + "\"");
 			}
 		}
 		add_to_file(APPS_FILE, "all_apps = " + str(count));
-		for (unsigned int i = 1; i <= count; i++)
-			names.insert(names.end(), configurator(APPS_FILE, str(i) + "_app_package_name", "", false));
-	} else {
-		count = atoi(configurator(APPS_FILE, "all_apps", "", false).c_str());
-		for (unsigned int i = 1; i <= count; i++)
-			names.insert(names.end(), configurator(APPS_FILE, str(i) + "_app_package_name", "", false));
+		
 	}
+
+	load_to_vector(APPS_FILE, apps_file_vec); // Загрузка файла со списком приложений
+	count = atoi(conf("all_apps", apps_file_vec).c_str()); // Сколько всего приложений
+	for (unsigned int i = 1; i <= count; i++)
+		names.insert(names.end(), conf(str(i) + "_app_package_name", apps_file_vec));
+
 	return 0;
 }
