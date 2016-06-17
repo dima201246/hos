1c2c5399450c7035d26fec5539e3a0fe4fc8d12a
Dmitriy Volchenko <dima201246@gmail.com>
2016-06-17 22:16:30 +0300

    Переделка под новые цвета

--- desktop/desktop.cpp
+++ desktop/desktop.cpp
@@ -1,57 +1,63 @@
-//#include "../shutdown/shutdown.h"
-#include "../windlg/windlg.h"
-#include "../fswork/fswork.h"
-#include "../apps_starter/apps_starter.h"
-#include "../time/time.h"
-
-#include "./header/menu_apps.h"
-
-#include <unistd.h>
-#include <stdlib.h>
-#include <curses.h>
+#include "desktop.h"
 
 using namespace std;
 
 void open_menu(vector <string>& app_list) {
-	DLGSTR menu_panel = {};
-	menu_panel.xpos = 1;
-	menu_panel.ypos = 1;
-	menu_panel.style = 3;
-	menu_panel.border_menu = true;
-	bool cycle = true;
-	int key;
+	DLGSTR		menu_panel	= {};
+
+	bool	cycle			= true;
+
+	int		key;
+
+	menu_panel.xpos			= 1;
+	menu_panel.ypos			= 1;
+	menu_panel.style		= BLUE_WIN;
+	menu_panel.border_menu	= true;	
+
 	while (cycle) {
 		menu_win(menu_panel, app_list);
-		key = getch();
+		key	= getch();
+		
 		switch (key) {
-			case 27: cycle = false; break;
-			case KEY_UP: if (menu_panel.selected != 1) menu_panel.selected--; break;
-			case KEY_DOWN: if (menu_panel.selected != menu_panel.second_border) menu_panel.selected++; break;
-			case '\n': app_start(menu_panel.selected, ""); return; break;
+			case 27:		cycle = false;
+							break;
+
+			case KEY_UP:	if (menu_panel.selected != 1)
+								menu_panel.selected--;
+							break;
+
+			case KEY_DOWN:	if (menu_panel.selected != menu_panel.second_border)
+								menu_panel.selected++;
+							break;
+
+			case '\n':		app_start(menu_panel.selected, "");
+							return;
+							break;
 		}
 	}
+
 	return;
 }
 
 int draw_desktop(int selected, bool open_label, unsigned int maxX, unsigned int maxY) {
-		for (unsigned int x = 0; x < maxX; x++) {
+		for (unsigned int	x	= 0; x < maxX; x++) {
 			mvprintw(maxY - 1, x, "-");
 			mvprintw(0, x, "-");
 		}
-		for (unsigned int y = 0; y < maxY; y++) {mvprintw(y, 0, "|"); mvprintw(y, maxX - 1, "|");}
+		for (unsigned int	y	= 0; y < maxY; y++) {mvprintw(y, 0, "|"); mvprintw(y, maxX - 1, "|");}
 
-		local_time	t_n = get_time_now();
+		local_time	t_n	= get_time_now();
 		mvprintw(0, maxX - 10, "%d:%d:%d", t_n.hours, t_n.min, t_n.sec);
 
-		if (selected == 0) attron(COLOR_PAIR(1) | A_BOLD); // Белый цвет
+		if (selected == 0) attron(COLOR_PAIR(TEXT_BLACK_WHITE) | A_BOLD);
 		mvprintw(0, 1, "Menu");
-		if (selected == 0) attroff(COLOR_PAIR(1) | A_BOLD);
-		if (selected == 1) attron(COLOR_PAIR(1) | A_BOLD); // Белый цвет
+		if (selected == 0) attroff(COLOR_PAIR(TEXT_BLACK_WHITE) | A_BOLD);
+		if (selected == 1) attron(COLOR_PAIR(TEXT_BLACK_WHITE) | A_BOLD);
 		mvprintw(0, 6, "Edit");
-		if (selected == 1) attroff(COLOR_PAIR(1) | A_BOLD);
-		if (selected == 2) attron(COLOR_PAIR(1) | A_BOLD); // Белый цвет
+		if (selected == 1) attroff(COLOR_PAIR(TEXT_BLACK_WHITE) | A_BOLD);
+		if (selected == 2) attron(COLOR_PAIR(TEXT_BLACK_WHITE) | A_BOLD);
 		mvprintw(0, 11, "Exit");
-		if (selected == 2) attroff(COLOR_PAIR(1) | A_BOLD);
+		if (selected == 2) attroff(COLOR_PAIR(TEXT_BLACK_WHITE) | A_BOLD);
 	return 0;
 }
 
@@ -61,30 +67,47 @@ int work_desktop(vector <string> app_list, string user_name) {
 	int key_pressed, selected = 0;
 	while (cycle) {
 		getmaxyx(stdscr, maxY, maxX); // Получение размера терминала
+
 		if ((maxX != maxXb) || (maxY != maxYb)) {
 			erase();
 		}
+
 		timeout(0);
 		draw_desktop(selected, open_label, maxX, maxY);
 		key_pressed = getch();
 		switch (key_pressed) {
-			case KEY_RIGHT: if (selected != 2) selected++; break;
-			case KEY_LEFT: if (selected != 0) selected--; break;
-			case '\n': switch (selected) {
-							case 0: open_menu(app_list); break;
-							case 1: break;
-							case 2: cycle = false; break;
-						}
-			case 27: if (open_label) open_label = false;
-					/*else shutdown_process();*/
-					break;
+			case KEY_RIGHT:	if (selected != 2)
+								selected++;
+							break;
+
+			case KEY_LEFT:	if (selected != 0)
+								selected--;
+							break;
+
+			case '\n':		switch (selected) {
+								case 0:	open_menu(app_list);
+										break;
+
+								case 1:	break;
+
+								case 2:	cycle	= false;
+										break;
+							}
+							break;
+
+			case 27:		if (open_label) {
+								open_label	= false;
+							}
+							/*else shutdown_process();*/
+							break;
 		}
 	}
+
 	return 0;
 }
 
 int main_desktop(string user_name/*...*/) {
-	vector <string> app_list;
+	vector <string>	app_list;
 	get_apps_list(app_list);
 	work_desktop(app_list, user_name);
 	return 0;
--- desktop/desktop.h
+++ desktop/desktop.h
@@ -1,5 +1,17 @@
 #ifndef DESKTOP_H
 #define DESKTOP_H
 	#include <string>
+	#include <unistd.h>
+	#include <stdlib.h>
+
+	#include "../windlg/windlg.h"
+	#include "../fswork/fswork.h"
+	#include "../apps_starter/apps_starter.h"
+	#include "../time/time.h"
+	#include "../screen/screen.h"
+
+	#include "header/menu_apps.h"
+
+
 	int main_desktop(std::string);
 #endif
