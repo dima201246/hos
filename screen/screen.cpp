0f60e971c96becdd0bf789d7c1f154eaf945694b
Dmitriy Volchenko <dima201246@gmail.com>
2016-07-18 18:25:53 +0300

    Важные изменения для работы библиотеки

--- screen/screen.cpp
+++ screen/screen.cpp
@@ -1,9 +1,9 @@
 #include "screen.h"
 
-int	main_system_color,
-	main_system_color_selection;
+int		main_system_color,
+		main_system_color_selection;
 
-void init_display() {
+void	init_display() {
 	initscr();
 	start_color();
 	keypad (stdscr, true);
@@ -12,7 +12,7 @@ void init_display() {
 	erase();
 }
 
-void init_color() {
+void	init_color() {
 	init_pair (TEXT_WHITE_BLACK, COLOR_WHITE, COLOR_BLACK);
 	init_pair (TEXT_WHITE_YELLOW, COLOR_WHITE, COLOR_YELLOW);
 	init_pair (TEXT_WHITE_BLUE, COLOR_WHITE, COLOR_BLUE);
@@ -42,9 +42,45 @@ void init_color() {
 	init_pair (TEXT_RED_WHITE, COLOR_RED, COLOR_WHITE);
 	init_pair (TEXT_MAGENTA_WHITE, COLOR_MAGENTA, COLOR_WHITE);
 	init_pair (TEXT_CYAN_WHITE, COLOR_CYAN, COLOR_WHITE);
+
+	init_pair (TEXT_BLUE_YELLOW, COLOR_BLUE, COLOR_YELLOW);
+	init_pair (TEXT_GREEN_YELLOW, COLOR_GREEN, COLOR_YELLOW);
+	init_pair (TEXT_RED_YELLOW, COLOR_RED, COLOR_YELLOW);
+	init_pair (TEXT_MAGENTA_YELLOW, COLOR_MAGENTA, COLOR_YELLOW);
+	init_pair (TEXT_CYAN_YELLOW, COLOR_CYAN, COLOR_YELLOW);
+
+	init_pair (TEXT_YELLOW_BLUE, COLOR_YELLOW, COLOR_BLUE);
+	init_pair (TEXT_GREEN_BLUE, COLOR_GREEN, COLOR_BLUE);
+	init_pair (TEXT_RED_BLUE, COLOR_RED, COLOR_BLUE);
+	init_pair (TEXT_MAGENTA_BLUE, COLOR_MAGENTA, COLOR_BLUE);
+	init_pair (TEXT_CYAN_BLUE, COLOR_CYAN, COLOR_BLUE);
+
+	init_pair (TEXT_YELLOW_GREEN, COLOR_YELLOW, COLOR_GREEN);
+	init_pair (TEXT_BLUE_GREEN, COLOR_BLUE, COLOR_GREEN);
+	init_pair (TEXT_RED_GREEN, COLOR_RED, COLOR_GREEN);
+	init_pair (TEXT_MAGENTA_GREEN, COLOR_MAGENTA, COLOR_GREEN);
+	init_pair (TEXT_CYAN_GREEN, COLOR_CYAN, COLOR_GREEN);
+
+	init_pair (TEXT_YELLOW_RED, COLOR_YELLOW, COLOR_RED);
+	init_pair (TEXT_BLUE_RED, COLOR_BLUE, COLOR_RED);
+	init_pair (TEXT_GREEN_RED, COLOR_GREEN, COLOR_RED);
+	init_pair (TEXT_MAGENTA_RED, COLOR_MAGENTA, COLOR_RED);
+	init_pair (TEXT_CYAN_RED, COLOR_CYAN, COLOR_RED);
+
+	init_pair (TEXT_YELLOW_MAGENTA, COLOR_YELLOW, COLOR_MAGENTA);
+	init_pair (TEXT_BLUE_MAGENTA, COLOR_BLUE, COLOR_MAGENTA);
+	init_pair (TEXT_GREEN_MAGENTA, COLOR_GREEN, COLOR_MAGENTA);
+	init_pair (TEXT_RED_MAGENTA, COLOR_RED, COLOR_MAGENTA);
+	init_pair (TEXT_CYAN_MAGENTA, COLOR_CYAN, COLOR_MAGENTA);
+
+	init_pair (TEXT_YELLOW_CYAN, COLOR_YELLOW, COLOR_CYAN);
+	init_pair (TEXT_BLUE_CYAN, COLOR_BLUE, COLOR_CYAN);
+	init_pair (TEXT_GREEN_CYAN, COLOR_GREEN, COLOR_CYAN);
+	init_pair (TEXT_RED_CYAN, COLOR_RED, COLOR_CYAN);
+	init_pair (TEXT_MAGENTA_CYAN, COLOR_MAGENTA, COLOR_CYAN);
 }
 
-void get_normal_inv_color(std::string color_str, int &color, int &sel_color) { // Получение кода цвета из имени и его инвертированной состовляющей
+void	get_normal_inv_color(std::string	color_str, color_t	&color, color_t	&sel_color) { // Получение кода цвета из имени и его инвертированной состовляющей
 	if (color_str == "cyan") {
 		color		= TEXT_CYAN_BLACK;
 		sel_color	= TEXT_BLACK_CYAN;
@@ -69,7 +105,7 @@ void get_normal_inv_color(std::string color_str, int &color, int &sel_color) { /
 	}
 }
 
-int get_inv_color(int color) { // Получение кода инвертированного цвета
+color_t	get_inv_color(color_t	color) { // Получение кода инвертированного цвета
 	if (color == TEXT_CYAN_BLACK) {
 		return TEXT_BLACK_CYAN;
 	} else if (color == TEXT_BLUE_BLACK) {
@@ -85,4 +121,23 @@ int get_inv_color(int color) { // Получение кода инвертиро
 	} else {
 		return TEXT_BLACK_WHITE;
 	}
+}
+
+color_t	get_color_on_color(color_t	color_top, color_t	color_bot) {
+
+	if ((color_top == 0) && (color_bot == 0)) {
+		return TEXT_BLACK_WHITE;
+	}
+
+	if ((color_top == color_bot) || (color_top == TEXT_WHITE_BLACK)) {
+		return ((color_bot + 1) * 7) + TEXT_WHITE_BLACK;
+	}
+
+	if (color_bot > color_top) {
+		return ((color_bot + 1) * 7) + (color_top + 1);
+	} else {
+		return ((color_bot + 1) * 7) + color_top;
+	}
+
+	return 0;
 }
--- screen/screen.h
+++ screen/screen.h
@@ -4,42 +4,91 @@
 	#include <curses.h>
 	#include <string>
 
+	/*Types*/
+	typedef int color_t;
+
 	extern	int	main_system_color,
 				main_system_color_selection;
 
 	void init_display();
 	void init_color();
-	void get_normal_inv_color(std::string /*Строка с названием цвета*/, int&/*Код цвета*/, int&/*Инвертированный цвет*/);
-	int get_inv_color(int);
+	void get_normal_inv_color(std::string /*Строка с названием цвета*/, color_t&/*Код цвета*/, color_t&/*Инвертированный цвет*/);
+	color_t get_inv_color(color_t);
+	color_t	get_color_on_color(color_t, color_t);
+
 
 	/*Colors*/
+/*	#define TEXT_WHITE_BLACK	0 = TEXT_WHITE_MAGENTA	42
+								  = TEXT_BLACK_MAGENTA	43
+	#define TEXT_YELLOW_BLACK	1 = TEXT_YELLOW_MAGENTA	44
+	#define TEXT_BLUE_BLACK		2 = TEXT_BLUE_MAGENTA	45
+	#define TEXT_GREEN_BLACK	3 = TEXT_GREEN_MAGENTA	46
+	#define TEXT_RED_BLACK		4 = TEXT_RED_MAGENTA	47
+	#define TEXT_MAGENTA_BLACK	5 = 
+	#define TEXT_CYAN_BLACK		6 = TEXT_CYAN_MAGENTA	48
+*/
 	#define TEXT_WHITE_BLACK	0
-	#define TEXT_WHITE_YELLOW	1
-	#define TEXT_WHITE_BLUE		2
-	#define TEXT_WHITE_GREEN	3
-	#define TEXT_WHITE_RED		4
-	#define TEXT_WHITE_MAGENTA	5
-	#define TEXT_WHITE_CYAN		6
+	#define TEXT_YELLOW_BLACK	1
+	#define TEXT_BLUE_BLACK		2
+	#define TEXT_GREEN_BLACK	3
+	#define TEXT_RED_BLACK		4
+	#define TEXT_MAGENTA_BLACK	5
+	#define TEXT_CYAN_BLACK		6
 
 	#define TEXT_BLACK_WHITE	7
-	#define TEXT_BLACK_YELLOW	8
-	#define TEXT_BLACK_BLUE		9
-	#define TEXT_BLACK_GREEN	10
-	#define TEXT_BLACK_RED		11
-	#define TEXT_BLACK_MAGENTA	12
-	#define TEXT_BLACK_CYAN		13
+	#define TEXT_YELLOW_WHITE	8
+	#define TEXT_BLUE_WHITE		9
+	#define TEXT_GREEN_WHITE	10
+	#define TEXT_RED_WHITE		11
+	#define TEXT_MAGENTA_WHITE	12
+	#define TEXT_CYAN_WHITE		13
 
-	#define TEXT_YELLOW_BLACK	15
-	#define TEXT_BLUE_BLACK		16
-	#define TEXT_GREEN_BLACK	17
-	#define TEXT_RED_BLACK		18
-	#define TEXT_MAGENTA_BLACK	19
-	#define TEXT_CYAN_BLACK		20
+	#define TEXT_WHITE_YELLOW	14
+	#define TEXT_BLACK_YELLOW	15
+	#define TEXT_BLUE_YELLOW	16
+	#define TEXT_GREEN_YELLOW	17
+	#define TEXT_RED_YELLOW		18
+	#define TEXT_MAGENTA_YELLOW	19
+	#define TEXT_CYAN_YELLOW	20
 
-	#define TEXT_YELLOW_WHITE	21
-	#define TEXT_BLUE_WHITE		22
-	#define TEXT_GREEN_WHITE	23
-	#define TEXT_RED_WHITE		24
-	#define TEXT_MAGENTA_WHITE	25
-	#define TEXT_CYAN_WHITE		26
-#endif  
+	#define TEXT_WHITE_BLUE		21
+	#define TEXT_BLACK_BLUE		22
+	#define TEXT_YELLOW_BLUE	23
+	#define TEXT_GREEN_BLUE		24
+	#define TEXT_RED_BLUE		25
+	#define TEXT_MAGENTA_BLUE	26
+	#define TEXT_CYAN_BLUE		27
+
+	#define TEXT_WHITE_GREEN	28
+	#define TEXT_BLACK_GREEN	29
+	#define TEXT_YELLOW_GREEN	30
+	#define TEXT_BLUE_GREEN		31
+	#define TEXT_RED_GREEN		32
+	#define TEXT_MAGENTA_GREEN	33
+	#define TEXT_CYAN_GREEN		34
+
+	#define TEXT_WHITE_RED		35
+	#define TEXT_BLACK_RED		36
+	#define TEXT_YELLOW_RED		37
+	#define TEXT_BLUE_RED		38
+	#define TEXT_GREEN_RED		39
+	#define TEXT_MAGENTA_RED	40
+	#define TEXT_CYAN_RED		41
+
+	#define TEXT_WHITE_MAGENTA	42
+	#define TEXT_BLACK_MAGENTA	43
+	#define TEXT_YELLOW_MAGENTA	44
+	#define TEXT_BLUE_MAGENTA	45
+	#define TEXT_GREEN_MAGENTA	46
+	#define TEXT_RED_MAGENTA	47
+	#define TEXT_CYAN_MAGENTA	48
+
+	#define TEXT_WHITE_CYAN		49
+	#define TEXT_BLACK_CYAN		50
+	#define TEXT_YELLOW_CYAN	51
+	#define TEXT_BLUE_CYAN		52
+	#define TEXT_GREEN_CYAN		53
+	#define TEXT_RED_CYAN		54
+	#define TEXT_MAGENTA_CYAN	55
+
+#endif
