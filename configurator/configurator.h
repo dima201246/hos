8723f8b0f1a73a16f815bd4d56bd3abdd61f9510
Dmitriy Volchenko <dima201246@gmail.com>
2016-07-18 18:27:37 +0300

    add_to_file defined as log_write

--- configurator/configurator.h
+++ configurator/configurator.h
@@ -5,6 +5,7 @@
 	#include <vector>
 	#include <string>
 	#include <stdlib.h>
+	#include "../system/system_defines.h"
 
 	extern	std::vector <std::string>	main_config_base;
 
@@ -12,5 +13,6 @@
 	void add_to_file(std::string /*Путь к файлу*/, std::string /*Строка, которая будет добавлена*/); // Запись в конец файла
 	std::string conf(std::string /*Искомый параметр*/, std::vector<std::string> /*вектор со значениями*/); //Поиск в векторе нужного параметра и возврат его значения
 	bool load_to_vector(std::string /*Имя файла*/, std::vector<std::string>&  /*Ссылка на вектор*/); // Загрузка файла в вектор
+	#define log_write(line)	add_to_file(MAIN_LOGFILE, line);
 
 #endif
