a50471bac202df572157dee8cf34bba23847d935
Dmitriy Volchenko <dima201246@gmail.com>
2016-08-09 21:10:18 +0300

    Unknown commit

--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,11 @@
 
 using namespace std;
 
-void button_obj(/*BUTSTR*/ void *button_conf, string text, color_t color_button) {
+void button_obj(/*BUTSTR*/ void *button_conf_point, string text, color_t color_button) {
+	BUTSTR	*button_conf	= (BUTSTR *)button_conf_point;
+	/*printw("posX: %d", button_conf->posX);
+	printw("posY: %d", button_conf->posY);*/
+	getch();
 	return;
 }
 
@@ -20,11 +24,41 @@ void add_to_win(vector<list_of_objects> &obj_list, win_object object_type, std::
 	obj_list.push_back(temp_value);
 }
 
-int main() {
-	return 0;
-}
-
 returned_str win(WINSTR* win_conf, std::vector<list_of_objects> obj_list) {
 	returned_str		returned_value;
 
+	list_of_objects		temp_item;
+
+	for (unsigned int	i	= 0; i < obj_list.size(); i++) {
+		erase();
+		temp_item	= obj_list[i];
+		printw("Item number: %d\n", i);
+		printw("Function point: %p\n", temp_item.point_to_function);
+		printw("Struct point: %p\n", temp_item.point_to_struct);
+		printw("Text: %s\n", temp_item.text.c_str());
+		temp_item.point_to_function(temp_item.point_to_struct, temp_item.text, temp_item.color_object);	// Вызов нужной функции
+		getch();
+	}
+}
+
+int main() {
+	initscr();
+	start_color();
+	keypad (stdscr, true);
+	noecho();
+	curs_set(0);
+	erase();
+
+	vector<list_of_objects> obj_list;
+
+	BUTSTR	test_button	= {};
+	test_button.posX	= 12;
+	test_button.posY	= 13;
+
+	add_to_win(obj_list, WIN_BUTTON, "TEST", 13, NULL);
+	add_to_win(obj_list, WIN_BUTTON, "TEST2", 14, &test_button);
+	win(NULL, obj_list);
+
+	endwin();
+	return 0;
 }
