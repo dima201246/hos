2dd6500baecb4819eb24560fbcd51cab66df94f5
Dmitriy Volchenko <dima201246@gmail.com>
2016-08-10 11:01:15 +0300

    Реструктура

--- test/test.cpp
+++ test/test.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-void button_obj(/*BUTSTR*/ void *button_conf_point, string text, color_t color_button) {
-	BUTSTR	*button_conf	= (BUTSTR *)button_conf_point;
+void button_obj(WINOBJ *button_conf, string text, color_t color_button) {
 
 	printw("\nIn button_obj: %p\nPoint in button: %p\n", &button_obj, button_conf);
 
@@ -16,24 +15,23 @@ void button_obj(/*BUTSTR*/ void *button_conf_point, string text, color_t color_b
 	return;
 }
 
-void add_to_win(vector<list_of_objects> &obj_list, win_object object_type, std::string text_on_object, color_t color_obj, void* point_to_conf) {
+void add_to_win(vector<list_of_objects> &obj_list, win_object object_type, std::string text_on_object, color_t color_obj, WINOBJ* point_to_conf) {
 	list_of_objects	temp_value	= {};
 
 	switch (object_type) {
 		case WIN_BUTTON:	temp_value.point_to_function	= &button_obj;
 							temp_value.type_obj				= WIN_BUTTON;
 
-							if (point_to_conf == NULL) {
-								BUTSTR	temp_button	= {};
-								temp_button.redraw	= true;
-								temp_value.point_to_struct		= &temp_button;
-							} else {
-								BUTSTR	*temp_button	= (BUTSTR *)point_to_conf;
-								temp_button->redraw	= true;
-								temp_value.point_to_struct		= temp_button;
-							}
 
 							break;
+	}
+
+	if (point_to_conf == NULL) {
+		WINOBJ	temp_button	= {};
+		temp_button.redraw	= true;
+		temp_value.point_to_struct	= &temp_button;
+	} else {
+		point_to_conf->redraw		= true;
 	}
 
 	temp_value.text					= text_on_object;
@@ -41,20 +39,43 @@ void add_to_win(vector<list_of_objects> &obj_list, win_object object_type, std::
 	obj_list.push_back(temp_value);
 }
 
-returned_str win(WINSTR* win_conf, std::vector<list_of_objects> obj_list) {
+returned_str win(WINOBJ* win_conf, std::vector<list_of_objects> obj_list) {
 	returned_str		returned_value;
 
 	list_of_objects		temp_item;
 
-	for (unsigned int	i	= 0; i < obj_list.size(); i++) {
-		erase();
-		temp_item	= obj_list[i];
-		printw("Item number: %d\n", i);
-		printw("Function point: %p\n", temp_item.point_to_function);
-		printw("Struct point: %p\n", temp_item.point_to_struct);
-		printw("Text: %s\n", temp_item.text.c_str());
-		temp_item.point_to_function(temp_item.point_to_struct, temp_item.text, temp_item.color_object);	// Вызов нужной функции
-		getch();
+
+	bool 				cycle,
+						first_write;
+
+
+	cycle				= true;
+	first_write			= true;
+
+	while (cycle) {
+		for (unsigned int	i	= 0; i < obj_list.size(); i++) {
+			temp_item	= obj_list[i];
+
+			#ifdef DEBUG
+			log_write("Object number: " + str(i));
+			log_write("Function point: 0x" + str(temp_item.point_to_function));
+			log_write("Struct point: 0x" + str(temp_item.point_to_struct));
+			log_write("Text: " + temp_item.text);
+			#endif
+
+			temp_item.point_to_function(temp_item.point_to_struct, temp_item.text, temp_item.color_object);	// Вызов нужного объекта
+
+/*			if (first_write) {
+				InitWINOBJ(temp_winobj);
+				win_object	= temp_item.point_to_struct;
+				win_object.redraw	= false;
+				obj_list[i]	= temp_item;
+			}*/
+
+		}
+
+		first_write	= false;
+		cycle		= false;
 	}
 }
 
@@ -68,7 +89,7 @@ int main() {
 
 	vector<list_of_objects> obj_list;
 
-	BUTSTR	test_button	= {};
+	InitWINOBJ(test_button);
 	test_button.posX	= 12;
 	test_button.posY	= 13;
 
--- test/test.h
+++ test/test.h
@@ -10,18 +10,26 @@
 	#define WIN_IPUT_TEXT	3
 	#define WIN_TOP_BAR		4
 	#define WIN_CHECKBOX	5
-	#define WIN_CHECKBOX	6
-	#define WIN_MENU		7
+	#define WIN_MENU		6
 
 	typedef	int	win_object;
 	typedef	int	color_t;
 
+	struct WINOBJ {
+		unsigned int	posX,
+						posY,
+						posXmax,
+						posYmax;
+
+		bool			redraw;
+	};
+
 	struct list_of_objects {
 		win_object	type_obj;
 
-		void		(*point_to_function)(void *, std::string, color_t);
+		void		(*point_to_function)(WINOBJ *, std::string, color_t);
 
-		void		*point_to_struct;
+		WINOBJ		*point_to_struct;
 
 		std::string	text;
 
@@ -45,25 +53,13 @@
 								returned_selected;	// Номер выделенного элемента, даже если окно было закрыто без выбора
 	};*/
 
-	struct BUTSTR {
-		unsigned int	posX,
-						posY;
-
-		bool			redraw;
-	};
-
-	struct WINSTR {
-		unsigned int	posX,
-						posY,
-						posXmax,
-						posYmax;
-	};
-
 	struct returned_str {
 		int				key;
 	};
 
-	void add_to_win(std::vector<list_of_objects>&, win_object, std::string, color_t, void*);
-	returned_str win(WINSTR*, std::vector<list_of_objects>);
+	#define InitWINOBJ(name_var)	WINOBJ	name_var	= {}
+
+	void add_to_win(std::vector<list_of_objects>&, win_object, std::string, color_t, WINOBJ*);
+	returned_str win(WINOBJ*, std::vector<list_of_objects>);
 
 #endif
