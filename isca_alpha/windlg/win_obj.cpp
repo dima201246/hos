0bc82e46dc15b0251b0c928e39b7fabe750a7210
Dmitriy Volchenko <dima201246@gmail.com>
2017-05-11 02:41:31 +0300

    Пилю текстовый объект

--- isca_alpha/windlg/win_obj.cpp
+++ isca_alpha/windlg/win_obj.cpp
@@ -1,4 +1,5 @@
 #include "internal_windlg.h"
+#include "../../include/isca_alpha.h"
 
 void get_obj_size(list_of_objects	item, unsigned int &x, unsigned int &y) {	// Получение предполагаемого размера объекта
 	if (item.point_to_struct->posXmax != 0)
@@ -64,22 +65,82 @@ int button_obj(WINOBJ *button_conf, std::string text, color_t color_button) {
 }
 
 /*TEXT FIELD START*/
+unsigned int textParcer(std::vector<std::string> &text_array, std::string text, unsigned int maxY, unsigned int last_string)
+{
+	unsigned int count = 0;
+
+	std::string temp_string = text;
+
+	unsigned int return_num = 0;
+
+	if ((text.length() != 0) && (text.length() > maxY))
+	{
+		return_num = ((double)((double)text.length() / (double)maxY) + 0.999999);
+	}
+
+	while (text.length() > maxY)
+	{
+		temp_string.erase(maxY, text.length());
+		text.erase(0, maxY);
+		text_array.push_back(temp_string);
+		temp_string = text;
+		count++;
+
+		if ((last_string != 0) && (count > last_string))
+			break;
+	}
+
+	if ((!text.empty()) && (count <= last_string))
+		text_array.push_back(text);
+
+	return return_num;
+}
+
 int text_obj(WINOBJ *obj_conf, std::string text, color_t color)
 {
+	std::vector<std::string> text_array;
+
+	clear_space(obj_conf->posX, obj_conf->posYdisplay, obj_conf->posXmax, obj_conf->posYmax);
+
 	if (obj_conf->redraw)
 	{
+
+		textParcer(text_array, text, obj_conf->posXmax - 5, 1);
+
 		if ((obj_conf->posXmax != 0) || (obj_conf->posYmax != 0)) {
-			clear_space(obj_conf->posX, obj_conf->posYdisplay, obj_conf->posXmax + 1, obj_conf->posYmax + 1);			
-			draw_box(1, "", 0, obj_conf->posX, obj_conf->posYdisplay, obj_conf->posXmax, obj_conf->posYmax, color, get_inv_color(color));
-			// mvprintw(obj_conf->posYdisplay, obj_conf->posX, "!%s!", text.c_str());
+			mvprintw(obj_conf->posYdisplay + 1, obj_conf->posX + 1, "%s...", text_array[0].c_str());
 		}
 	}
 	else
 	{
-		return getch();
+		timeout(-1);
+
+		int key = 0;
+
+		unsigned int how_many = textParcer(text_array, text, obj_conf->posXmax - 2, obj_conf->posYmax - 3);
+		
+		draw_box(1, "", (how_many > (obj_conf->posYmax - 2) ? 1 : 0), obj_conf->posX, obj_conf->posYdisplay, obj_conf->posXmax, obj_conf->posYmax - 1, color, get_inv_color(color));
+
+		for (unsigned int i = 0; i < text_array.size(); ++i)
+		{
+			mvprintw(obj_conf->posYdisplay + 1 + i, obj_conf->posX + 1, "%s", text_array[i].c_str());
+		}
+
+		key = getch();
+
+		while ((key != H_KEY_ESC) && (key != H_KEY_TAB) && (key != KEY_LEFT) && (key != KEY_RIGHT))
+		{
+			/*switch (key)
+			{
+				case KEY_DOWN: if ()
+			}*/
+
+			key = getch();
+		}
+
+		return key;
 	}
 
 	return 0;
-	// return button_obj(obj_conf, text, color);
 }
 /*TEXT FIELD END*/
